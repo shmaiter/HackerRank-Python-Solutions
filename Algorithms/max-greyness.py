ea0ff1ad8b7510c97696a8f0cd0a2d4f7e7263a7
Shmaither <shmaither@gmail.com>
2022-10-09 14:41:40 -0600

    new: add pixel greyness formula and max level obtained on the grid

--- Algorithms/max-greyness.py
+++ Algorithms/max-greyness.py
@@ -19,41 +19,55 @@ the result will always be equal to the numerator.
 # [print(f'Position for {i}: is column {i % 3}') for i in range(12)]
 
 
-pixels = [[1, 1, 0],
-          [0, 0, 1],
+# pixels = [[1, 0, 1],
+#           [0, 0, 1],
+#           [1, 1, 0]]
+pixels = [[1, 1, 1],
+          [0, 1, 1],
           [1, 1, 0]]
 
 
-def max_greyness(cell):
-    cell = cell - 1 if cell != 0 else cell
-    print(f'cell: {cell}')
-    length = len(pixels[0])
-    row_area = list()
-    col_area = list()
-    cell_row_index = cell // length
-    cell_col_index = cell % length
+def max_greyness():
+    max_level = 0
+    for cell in range(len(pixels)*len(pixels[0])):
+        cell = cell - 1 if cell != 0 else cell
+        length = len(pixels[0])
 
-    ''' Comprehension List Short Version '''
-    # row_area = [value for index, value in enumerate(
-    #     pixels) if index == cell_row_index]
-    # col_area = [
-    #     value for row in pixels for index, value in enumerate(row) if index == cell_col_index]
+        # Step 1: sort the cell position according to the row and column
+        cell_row_index = cell // length
+        cell_col_index = cell % length
 
-    for index, value in enumerate(pixels):
-        if index == cell_row_index:
-            row_area.extend(value)
+        # Step 2: Extract the numbers before, after, up and down the selected cell.
+        row_area = list()
+        col_area = list()
 
-    for row in pixels:
-        for index, value in enumerate(row):
-            print(
-                f'index: {index} value: {value} cell_col_ind: {cell_col_index}')
-            if index == cell_col_index:
-                col_area.append(value)
+        for index, value in enumerate(pixels):
+            if index == cell_row_index:
+                row_area.extend(value)
 
-    print(f'Row area: {row_area}')
-    print(f'Column area: {col_area}')
+        for row in pixels:
+            for index, value in enumerate(row):
+                if index == cell_col_index:
+                    col_area.append(value)
+
+        ''' Comprehension List Short Version '''
+        # row_area = [value for index, value in enumerate(
+        #     pixels) if index == cell_row_index]
+        # col_area = [
+        #     value for row in pixels for index, value in enumerate(row) if index == cell_col_index]
+
+        # Step 3: Sum up the 1's and 0's and apply the formula for levels of grayness
+        # g = (1's + 1's) - (0's + 0's)
+
+        grey_level = (sum(row_area) + sum(col_area)) - \
+            ((length - sum(row_area))+(length - sum(col_area)))
+
+        # Step 5: Print the max greyness level
+        max_level = grey_level if grey_level > max_level else max_level
+
+    return max_level
 
 
 if __name__ == '__main__':
-    cell = 1
-    max_greyness(cell)
+
+    print(max_greyness())
